6c8e8429964ce32c1097d3245598770b926a3072
agaier <adam.ga@gmail.com>
2022-08-05 14:28:56 +0200

    CVT also works with objects

--- bbq/archives/_init_archive.py
+++ bbq/archives/_init_archive.py
@@ -5,15 +5,19 @@ from bbq.archives._archive_base import BBQArchiveBase
 from ribs.archives._cvt_archive import CVTArchive
 from ribs.archives._grid_archive import GridArchive
 
-class BbqGrid(GridArchive, BBQArchiveBase):
+#class BbqGrid(GridArchive, BBQArchiveBase):
+class BbqGrid(BBQArchiveBase, GridArchive):
     def __init__(self, grid_res=None, desc_bounds=None, seed=None, use_objects=False, **_):    
         self.use_objects = use_objects
         super().__init__(grid_res, desc_bounds)
+        #self.initialize = BBQArchiveBase.initialize
 
-class BbqCVT(CVTArchive, BBQArchiveBase):
+
+class BbqCVT(BBQArchiveBase, CVTArchive):
     def __init__(self, n_bins=None, desc_bounds=None, seed=None, use_objects=False, **_):    
         self.use_objects = use_objects
         super().__init__(n_bins, desc_bounds)
+        #self.initialize = BBQArchiveBase.initialize
 
 
 archive_lookup = {'Grid'    : BbqGrid,
